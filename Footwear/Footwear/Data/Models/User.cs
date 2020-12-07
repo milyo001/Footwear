9ec1c479d1d3943c06ed01b336dd26291407726e
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2020-12-07 20:28:11 +0200

    Add password property for User model

--- Footwear/Footwear/Data/Models/User.cs
+++ Footwear/Footwear/Data/Models/User.cs
@@ -13,6 +13,9 @@
         [Required]
         public string LastName { get; set; }
 
+        [Required]
+        public string Password { get; set; }
+
         [Required, EmailAddress]
         public string Email { get; set; }
 
