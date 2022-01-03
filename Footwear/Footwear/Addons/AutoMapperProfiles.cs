0959186feb061a807ee5df28ce8c3618fc93ef8d
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-01-03 21:33:50 +0200

    Change target framework back to .NET 5

---
+++ Footwear/Footwear/Addons/AutoMapperProfiles.cs
@@ -0,0 +1,14 @@
+﻿namespace Footwear.Addons
+{
+    using AutoMapper;
+    using Footwear.Data.Dto;
+    using Footwear.Data.Models;
+
+    public class AutoMapperProfile : Profile
+    {
+        public void RegisterUserProfile()
+        {
+            CreateMap<RegisterViewModel, User>();
+        }
+    }
+}
