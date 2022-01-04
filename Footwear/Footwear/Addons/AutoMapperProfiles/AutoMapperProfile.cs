e67ecc5e7a74796efbea45dde0f9cebe094d5375
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-01-04 22:28:37 +0200

    Add mapping configuration for updating user information

--- Footwear/Footwear/Addons/AutoMapperProfiles/AutoMapperProfile.cs
+++ Footwear/Footwear/Addons/AutoMapperProfiles/AutoMapperProfile.cs
@@ -15,9 +15,15 @@
                 opt.MapFrom(src => new Address { City = "", Street = "", Country = "", State = "", ZipCode = "" }))
                 .ForMember(dest => dest.Cart, opt => opt.MapFrom(src => new Cart { }));
 
-            CreateMap<UserProfileDataViewModel, User>();
-        }
+            //Modfiy user information
+            CreateMap<UserProfileDataViewModel, User>()
+                .ForMember(dest => dest.Address.Street, opt => opt.MapFrom(src => src.Street))
+                .ForMember(dest => dest.Address.City, opt => opt.MapFrom(src => src.City))
+                .ForMember(dest => dest.Address.State, opt => opt.MapFrom(src => src.State))
+                .ForMember(dest => dest.Address.ZipCode, opt => opt.MapFrom(src => src.ZipCode))
+                .ForMember(dest => dest.Address.Country, opt => opt.MapFrom(src => src.Country));
+                
 
-       
+        }       
     }
 }
--- Footwear/Footwear/Services/UserService/IUserService.cs
+++ Footwear/Footwear/Services/UserService/IUserService.cs
@@ -11,5 +11,6 @@ namespace Footwear.Services.UserService
 
         Task<IdentityResult> CreateUserAsync(RegisterViewModel model);
 
+
     }
 }
