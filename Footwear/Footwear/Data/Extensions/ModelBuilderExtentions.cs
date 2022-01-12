498c5879557983cf19e014dc702999c91ccd50a8
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-01-12 22:40:14 +0200

    Add a model builder extension class and a folder for all future extentions

---
+++ Footwear/Footwear/Data/Extensions/ModelBuilderExtentions.cs
@@ -0,0 +1,15 @@
+﻿namespace Footwear.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Footwear.Data.Models
+
+    //An extention of ModelBuilder class. "this" keyword tells the compiler that this particular Extension Method should be added to objects of type ModelBuilder
+    public static class ModelBuilderExtentions
+    {
+        //A method for seeding some products into the database
+        public static void SeedProducts(this ModelBuilder  builder)
+        {
+            builder.Entity<Product>().HasData();
+        }
+    }
+}
