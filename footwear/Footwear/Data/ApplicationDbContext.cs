b7be6e67426ead9845e5b96029d956500cd49862
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-01-13 14:18:58 +0200

    Remove base.OnModelCreating, calling it is not necessary

---
+++ footwear/Footwear/Data/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿namespace Footwear.Data
+{
+    using Footwear.Data.Models;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<AppData> AppData { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Cart> Cart { get; set; }
+
+        public DbSet<CartProduct> CartProducts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Address> Addresses { get; set; }
+
+        public DbSet<ProductImage> ProductsImage { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //Seeds the database with data
+            builder.Seed();
+        }
+
+
+
+    }
+}
