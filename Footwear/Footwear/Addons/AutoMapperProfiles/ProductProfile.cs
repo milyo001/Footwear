b6cf8a23569e842e0d4665df053714a63daf66e1
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-02-07 13:17:21 +0200

    Add new product service and AutoMapper Product Profile

---
+++ Footwear/Footwear/Addons/AutoMapperProfiles/ProductProfile.cs
@@ -0,0 +1,12 @@
+﻿namespace Footwear.Addons.AutoMapperProfiles
+{
+    using AutoMapper;
+
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<RegisterViewModel, User>()
+        }
+    }
+}
