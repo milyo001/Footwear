507bd08cca1b982531ae117f2a26f748039983da
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-02-08 13:32:15 +0200

    Clean up some using statements

--- Footwear/Footwear/Services/CartService/CartService.cs
+++ Footwear/Footwear/Services/CartService/CartService.cs
@@ -3,10 +3,8 @@
     using Footwear.Data;
     using Footwear.ViewModels;
     using Footwear.Data.Models;
-    using Footwear.Data.Models.Enums;
     using Footwear.Services.TokenService;
     using Microsoft.EntityFrameworkCore;
-    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
