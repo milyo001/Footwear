a03f3ed0d63a905c9a9cc2c78b3481f516e8932e
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-02-15 14:31:05 +0200

    Add xml documentation for order interface

--- Footwear/Footwear/Services/OrderService/IOrderService.cs
+++ Footwear/Footwear/Services/OrderService/IOrderService.cs
@@ -8,20 +8,59 @@ namespace Footwear.Services.OrderService
 
     public interface IOrderService
     {
+        /// <summary>
+        /// Creates an order by given jwt auth token and view model.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="order"></param>
+        /// <returns></returns>
         Task CreateOrderAsync(string token, OrderViewModel order);
 
+        /// <summary>
+        /// Will returns latest added order id by given auth token.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
         Task<string> GetLatestAddedOrderIdAsync(string token);
 
+        /// <summary>
+        /// Gets latest added order for user.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
         Task<Order> GetLatestAddedOrderAsync(string token);
 
+        /// <summary>
+        /// Finds and returns an order with given id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Task<Order> GetOrderByIdAsync(string id);
 
+        /// <summary>
+        /// Change payment status of order to paid.
+        /// </summary>
+        /// <param name="orderId"></param>
         void ModifyPaidOrder(string orderId);
 
+
+        /// <summary>
+        /// Gets all information about the delivery.
+        /// </summary>
+        /// <returns></returns>
         Task<DeliveryInfoViewModel> GetDeliveryDataAsync();
 
+        /// <summary>
+        /// Calculate and return the total price for all cart products.
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
         double GetTotalPrice(Order order);
 
+        /// <summary>
+        /// Gets delivery price.
+        /// </summary>
+        /// <returns></returns>
         Task<double> GetDeliveryPriceAsync();
     }
 }
