287536d71b206e9e373108c9e6e2dadd5d845de2
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-02-16 11:05:11 +0200

    Add documentation for the token interface

--- Footwear/Footwear/Services/TokenService/ITokenService.cs
+++ Footwear/Footwear/Services/TokenService/ITokenService.cs
@@ -5,10 +5,40 @@
 
     public interface ITokenService
     {
+        /// <summary>
+        /// Returns user id by given auth token
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
         string GetUserId(string token);
+
+        /// <summary>
+        /// Returns cart id by given auth token
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
         int GetCartId(string token);
+
+        /// <summary>
+        /// Returns user entity by given auth token
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
         Task<User> GetUserByIdAsync(string token);
+
+        /// <summary>
+        /// Generate login token 
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="cartId"></param>
+        /// <returns></returns>
         string GenerateToken(string userId, int cartId);
+
+        /// <summary>
+        /// Encrypt the JWT token to prevent the user from accessing the JWT token claims
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
         string EncryptToken(string token);
     }
 }
