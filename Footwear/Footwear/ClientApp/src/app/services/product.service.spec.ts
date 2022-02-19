e53d48fde353556966b3e3271d4ebfbb5403dabf
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-02-19 23:45:16 +0200

    Code clean up unit tests

--- Footwear/Footwear/ClientApp/src/app/services/product.service.spec.ts
+++ Footwear/Footwear/ClientApp/src/app/services/product.service.spec.ts
@@ -4,27 +4,23 @@ import { ProductService } from './product.service';
 
 describe('ProductService', () => {
 
+  const service: ProductService = TestBed.get(ProductService);
+
   beforeEach(() => TestBed.configureTestingModule({
     imports: [HttpClientModule],
     providers: [{ provide: 'BASE_URL', useValue: 'http://localhost' }]
   }));
 
   it('should be created', () => {
-    const service: ProductService = TestBed.get(ProductService);
     expect(service).toBeTruthy();
   });
 
   it('Method getAllProducts should be created', () => {
-    const service: ProductService = TestBed.get(ProductService);
     expect(service.getAllProducts).toBeTruthy();
   });
 
   it('Method getProductById should be created', () => {
-    const service: ProductService = TestBed.get(ProductService);
     expect(service.getProductById).toBeTruthy();
   });
-
-  
-  
 
 });
