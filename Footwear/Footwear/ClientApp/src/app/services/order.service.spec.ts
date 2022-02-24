c75e0e6ac0bcae1514cc824bdea65c942b86337d
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-02-24 13:07:16 +0200

    Add unit test for create order method with a http spy

--- Footwear/Footwear/ClientApp/src/app/services/order.service.spec.ts
+++ Footwear/Footwear/ClientApp/src/app/services/order.service.spec.ts
@@ -1,6 +1,9 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { getBaseUrl } from '../../environments/environment.test';
+import { IOrder } from '../interfaces/order/order';
+import { IUserData } from '../interfaces/user/userData';
+import { asyncData } from '../testing/async-observable-helpers';
 
 import { OrderService } from './order.service';
 
@@ -36,5 +39,38 @@ describe('OrderService', () => {
   it('#checkOut should be created', () => {
     expect(service.checkOut).toBeTruthy();
   });
-  
+
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post']);
+    service = new OrderService(httpClientSpy, getBaseUrl());
+  });
+
+  it('#createOrder should return expected to return boolean property cardPayment (HttpClient called just once)', (done: DoneFn) => {
+
+    const userData: IUserData = {
+      firstName: "Miroslav", lastName: "Ilyovski", street: "Rakovska 3",
+      state: "Sofia", city: "Sofia", country: "Bulgaria",
+      email: "miroslavilyoooovski@gmail.net", phone: "089422123565", zipCode: "1022"
+    };
+    const order: IOrder = {
+      status: 'pending',
+      createdOn: "02/02/2022",
+      payment: 'card',
+      userData: userData
+    };
+    const expectedResult = { cardPayment: true };
+
+    httpClientSpy.post.and.returnValue(asyncData(order));
+
+    service.createOrder(order)
+      .subscribe(data => {
+        expect(expectedResult)
+          .withContext('expected boolean property cardPayment')
+          .toEqual(expectedResult);
+        done();
+      }), (err => done.fail);
+    expect(httpClientSpy.post.calls.count())
+      .withContext('one call')
+      .toBe(1);
+  });
 });
