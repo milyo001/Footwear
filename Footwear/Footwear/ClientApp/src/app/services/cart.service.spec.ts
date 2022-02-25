3244665b0f7ca333c4db099e2ed14ff9b2ee1485
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-02-25 22:53:48 +0200

    Add more tests in cart service

--- Footwear/Footwear/ClientApp/src/app/services/cart.service.spec.ts
+++ Footwear/Footwear/ClientApp/src/app/services/cart.service.spec.ts
@@ -1,9 +1,42 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { getBaseUrl } from '../../environments/environment.test';
 
 import { CartService } from './cart.service';
 
 describe('CartService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: CartService;
+  const baseUrl = getBaseUrl();
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule],
+    providers: [{ provide: 'BASE_URL', useValue: baseUrl }]
+  }));
+
+  it('Service should be created', () => {
+    const service: CartService = TestBed.get(CartService);
+    expect(service).toBeTruthy();
+  });
+
+  it('#addToCart should be created', () => {
+    const service: CartService = TestBed.get(CartService);
+    expect(service.addToCart).toBeTruthy();
+  });
+
+  it('#defaultQuantity should be created', () => {
+    const service: CartService = TestBed.get(CartService);
+    expect(service.defaultQuantity).toBeTruthy();
+  });
+
+  it('should be created', () => {
+    const service: CartService = TestBed.get(CartService);
+    expect(service).toBeTruthy();
+  });
+
+  it('should be created', () => {
+    const service: CartService = TestBed.get(CartService);
+    expect(service).toBeTruthy();
+  });
 
   it('should be created', () => {
     const service: CartService = TestBed.get(CartService);
