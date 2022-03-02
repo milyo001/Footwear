15d4aa15fbcf987850960cbccbe243e532f812ab
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-03-02 11:51:54 +0200

    Add more unit tests, remove HttpClientModule, import HttpClientTestingModule

--- Footwear/Footwear/ClientApp/src/app/user/user-profile/user-profile.component.spec.ts
+++ Footwear/Footwear/ClientApp/src/app/user/user-profile/user-profile.component.spec.ts
@@ -1,25 +1,72 @@
+import { HttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { getBaseUrl } from '../../../environments/environment.test';
+import { SharedModule } from '../../modules/shared.module';
+import { UserService } from '../../services/user.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 
 import { UserProfileComponent } from './user-profile.component';
 
 describe('UserProfileComponent', () => {
+
   let component: UserProfileComponent;
   let fixture: ComponentFixture<UserProfileComponent>;
 
+  // Mock User service properties
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  const baseUrl = getBaseUrl();
+  httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post']);
+  let mockUserService: UserService = new UserService(httpClientSpy, getBaseUrl());
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserProfileComponent ]
+      declarations: [UserProfileComponent],
+      imports: [BrowserAnimationsModule, HttpClientTestingModule, SharedModule, ToastrModule.forRoot()],
+      providers: [
+      { provide: 'BASE_URL', useValue: baseUrl },
+       ToastrService,
+        { provide: UserService, userValue: mockUserService }
+      ]
     })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
+      .compileComponents();
     fixture = TestBed.createComponent(UserProfileComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Should create Component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('#changeEmail should create', () => {
+    expect(component.changeEmail).toBeTruthy();
+  });
+
+  it('#changePassword should create', () => {
+    expect(component.changePassword).toBeTruthy();
+  });
+
+  it('#loadDataAsync should create', () => {
+    expect(component.loadDataAsync).toBeTruthy();
+  });
+
+  it('#ngAfterViewInit should be inherited and created', () => {
+    expect(component.ngAfterViewInit).toBeTruthy();
+  });
+
+  it('#ngOnInit should be inherited and created', () => {
+    expect(component.ngOnInit).toBeTruthy();
+  });
+
+  it('#setEmailFormValidation should be created', () => {
+    expect(component.setEmailFormValidation).toBeTruthy();
+  });
+
+  it('#setPasswordFormValidation should be created', () => {
+    expect(component.setPasswordFormValidation).toBeTruthy();
+  });
+
 });
