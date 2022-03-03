f5d8d4814ca0bcc4b0b43f3088ea4ff4c1ddbcfb
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-03-03 22:51:55 +0200

    Add tests for AuthGuard

--- Footwear/Footwear/ClientApp/src/app/interceptors/auth.guard.spec.ts
+++ Footwear/Footwear/ClientApp/src/app/interceptors/auth.guard.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
+import { Router  } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CookieService } from 'ngx-cookie-service';
+import { IndividualConfig, ToastrModule, ToastrService } from 'ngx-toastr';
 
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
-  let guard: AuthGuard;
+    let guard: AuthGuard;
+    // Make test toastr service to inject it into TestBed
+    const toastrService = {
+        success: (
+            message?: string,
+            title?: string,
+            override?: Partial<IndividualConfig>
+        ) => { },
+        error: (
+            message?: string,
+            title?: string,
+            override?: Partial<IndividualConfig>
+        ) => { },
+    };
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    guard = TestBed.inject(AuthGuard);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [RouterTestingModule, ToastrModule],
+            providers: [RouterTestingModule, CookieService,
+                { provide: ToastrService, useValue: toastrService }]
+        });
+        guard = TestBed.inject(AuthGuard);
+    });
 
-  it('should be created', () => {
-    expect(guard).toBeTruthy();
-  });
+    it('AuthGuard should be created', () => {
+        expect(guard).toBeTruthy();
+    });
+
+    it('#canActivate should be created', () => {
+        expect(guard.canActivate).toBeTruthy();
+    });
+
 });
