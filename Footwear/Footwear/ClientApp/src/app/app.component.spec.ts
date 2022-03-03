3445b246ec45d7ee8d9db4f77e6ffd81b3569cc1
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-03-03 19:46:30 +0200

    Add test to app component

--- Footwear/Footwear/ClientApp/src/app/app.component.spec.ts
+++ Footwear/Footwear/ClientApp/src/app/app.component.spec.ts
@@ -1,37 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { SharedModule } from './modules/shared.module';
+import { LoadingService } from './services/loading.service';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        SharedModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule,
+                SharedModule,
+            ],
+            declarations: [
+                AppComponent
+            ],
+            providers: [LoadingService]
+        }).compileComponents();
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
+    });
 
-  it(`should have as title 'ClientApp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ClientApp');
-  });
+    it(`should have as title 'Footwear'`, () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app.title).toEqual('Footwear');
+    });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ClientApp app is running!');
-  });
+    it('should have loading Observable', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app.loading).toBeTruthy();
+    });
+
+    it('should not have mat-progress-spinner component(while not fetching from API)', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement;
+        expect(compiled.querySelector('mat-progress-spinner')).toBeFalsy();
+    });
 });
