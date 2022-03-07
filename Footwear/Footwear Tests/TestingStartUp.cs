4d3669062f83883239b83db20fa8cd5f2ce7fe80
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-03-07 13:22:35 +0200

    Remove dependencies

--- Footwear/Footwear Tests/TestingStartUp.cs
+++ Footwear/Footwear Tests/TestingStartUp.cs
@@ -1,13 +1,6 @@
 ﻿namespace MusicStore.Test
 {
     using Footwear;
-    using Footwear.Data.Models;
-    using Footwear.Services.CartService;
-    using Footwear.Services.OrderService;
-    using Footwear.Services.ProductService;
-    using Footwear.Services.TokenService;
-    using Footwear.Services.UserService;
-    using Microsoft.AspNetCore.Identity;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Moq;
