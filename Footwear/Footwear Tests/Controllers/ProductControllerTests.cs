865ad51de8dcb52f7208c3b7747fcd36bc2de3b0
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-03-11 15:05:47 +0200

    Add more tests for add to cart method

--- Footwear/Footwear Tests/Controllers/ProductControllerTests.cs
+++ Footwear/Footwear Tests/Controllers/ProductControllerTests.cs
@@ -1,9 +1,11 @@
 ﻿namespace Footwear_Tests.Controllers
 {
     using Footwear.Controllers;
+    using Footwear.Data.Models;
     using Footwear.Services.CartService;
     using Footwear.Services.ProductService;
     using Footwear.ViewModels;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
     using Moq;
     using System.Collections.Generic;
@@ -61,7 +63,7 @@
         }
 
         [Fact]
-        public void Test_Test_GetProductDtoByIdAsync_Returns_NotFound_When_Null()
+        public void Test_GetProductDtoByIdAsync_Returns_NotFound_When_Null()
         {
             var testProducts = new ProductDto();
             this.ProductServiceMock.Setup(p => p.GetProductDtoByIdAsync(1))
@@ -71,5 +73,64 @@
             var result = testController.GetProductDtoById(1).Result;
             Assert.IsType<NotFoundResult>(result.Result);
         }
+
+        [Fact]
+        public void Test_AddCartProduct_Works_As_Expected()
+        {
+            var product = new Product()
+            {
+                Id = 1,
+            };
+            var httpContext = new DefaultHttpContext();
+            httpContext.Items.Add("token", "test");
+
+            this.ProductServiceMock
+                .Setup(p => p.GetProductByIdAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult(product));
+            this.CartServiceMock
+                .Setup(c => c.AddCartProductAsync("tesasdweq", product, 44));
+
+            var testController = new ProductController(this.CartServiceMock.Object, this.ProductServiceMock.Object)
+            {
+                ControllerContext = new ControllerContext() { HttpContext = httpContext }
+            };
+
+            var result = testController.AddCartProduct(new AddToCartModel());
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void Test_AddCartProduct_ModelState_Invalid_Returning_BadRequest()
+        {
+            var testController = new ProductController(this.CartServiceMock.Object, this.ProductServiceMock.Object) { };
+            testController.ModelState.AddModelError("testError", "Noooooooo");
+            var result = testController.AddCartProduct(new AddToCartModel());
+            Assert.IsType<BadRequestObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void Test_AddCartProduct_Returns_BadRequestObjectResult_When_Product_Is_Null()
+        {
+            var product = new Product()
+            {
+                Id = 1,
+            };
+            var httpContext = new DefaultHttpContext();
+            httpContext.Items.Add("token", "test");
+
+            this.ProductServiceMock
+                .Setup(p => p.GetProductByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync((Product)null);
+
+            var testController = new ProductController(this.CartServiceMock.Object, this.ProductServiceMock.Object)
+            {
+                ControllerContext = new ControllerContext() { HttpContext = httpContext }
+            };
+
+            var result = testController.AddCartProduct(new AddToCartModel());
+            Assert.IsType<BadRequestObjectResult>(result.Result);
+        }
+
+
     }
 }
