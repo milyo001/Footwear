16a5d43d8da7cc439a7ff2b953c506a2a03c3878
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-03-11 13:07:36 +0200

    Add new test for Products Controller methods

--- Footwear/Footwear Tests/Controllers/ProductControllerTests.cs
+++ Footwear/Footwear Tests/Controllers/ProductControllerTests.cs
@@ -1,13 +1,42 @@
 ﻿namespace Footwear_Tests.Controllers
 {
-    using System;
+    using Footwear.Controllers;
+    using Footwear.Services.CartService;
+    using Footwear.Services.ProductService;
+    using Footwear.ViewModels;
+    using Microsoft.AspNetCore.Mvc;
+    using Moq;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using System.Threading.Tasks;
+    using Xunit;
 
     public class ProductControllerTests
     {
+        public Mock<IProductService> ProductServiceMock { get; set; }
+        public Mock<ICartService> CartServiceMock { get; set; }
 
+        public ProductControllerTests()
+        {
+            this.ProductServiceMock = new Mock<IProductService>();
+            this.CartServiceMock = new Mock<ICartService>();
+        }
+
+        [Fact]
+        public void Test_GetAllProducts_ReturnType()
+        {
+            var testController = new ProductController(this.CartServiceMock.Object, this.ProductServiceMock.Object);
+            var result = testController.GetAllProducts();
+
+            Assert.IsAssignableFrom<Task<ActionResult<IEnumerable<ProductDto>>>>(result);
+        }
+
+        [Fact]
+        public void Test_GetProductDtoById_ReturnType()
+        {
+            var testController = new ProductController(this.CartServiceMock.Object, this.ProductServiceMock.Object);
+            var result = testController.GetProductDtoById(1);
+
+            Assert.IsAssignableFrom<Task<ActionResult<ProductDto>>>(result);
+        }
     }
 }
