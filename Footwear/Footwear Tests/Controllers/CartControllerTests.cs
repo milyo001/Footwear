c30cf24a827ac5b58d1886c6f2c2047e61a85136
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-03-14 10:00:44 +0200

    Add more tests for cart controller

--- Footwear/Footwear Tests/Controllers/CartControllerTests.cs
+++ Footwear/Footwear Tests/Controllers/CartControllerTests.cs
@@ -2,14 +2,65 @@
 
 namespace Footwear_Tests.Controllers
 {
-    using System;
+    using Footwear.Controllers;
+    using Footwear.Services.CartService;
+    using Footwear.Services.TokenService;
+    using Footwear.ViewModels;
+    using Microsoft.AspNetCore.Mvc;
+    using Moq;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using System.Threading.Tasks;
+    using Xunit;
 
     public class CartControllerTests
     {
+        public Mock<ICartService> CartServiceMock { get; set; }
+        public Mock<ITokenService> TokenServiceMock { get; set; }
 
+        public CartControllerTests()
+        {
+            this.CartServiceMock = new Mock<ICartService>();
+            this.TokenServiceMock = new Mock<ITokenService>();
+        }
+
+        [Fact]
+        public void Test_GetCartProductsAsync_ReturnType()
+        {
+            var testController = new CartController(this.TokenServiceMock.Object, this.CartServiceMock.Object);
+            var result = testController.GetCartProductsAsync();
+            Assert.IsAssignableFrom<Task<IEnumerable<CartProductViewModel>>>(result);
+        }
+
+        [Fact]
+        public void Test_IncrementCartProductQuantity_ReturnType()
+        {
+            var testController = new CartController(this.TokenServiceMock.Object, this.CartServiceMock.Object);
+            var result = testController.IncrementCartProductQuantity(1);
+            Assert.IsAssignableFrom<Task<IActionResult>>(result);
+        }
+
+        [Fact]
+        public void Test_DecreaseCartProductQuantity_ReturnType()
+        {
+            var testController = new CartController(this.TokenServiceMock.Object, this.CartServiceMock.Object);
+            var result = testController.DecreaseCartProductQuantity(1);
+            Assert.IsAssignableFrom<Task<IActionResult>>(result);
+        }
+
+        [Fact]
+        public void Test_DeleteCartProduct_ReturnType()
+        {
+            var testController = new CartController(this.TokenServiceMock.Object, this.CartServiceMock.Object);
+            var result = testController.DeleteCartProduct(1);
+            Assert.IsAssignableFrom<Task<IActionResult>>(result);
+        }
+
+        [Fact]
+        public void Test_RemoveCartProducts_ReturnType()
+        {
+            var testController = new CartController(this.TokenServiceMock.Object, this.CartServiceMock.Object);
+            var result = testController.RemoveCartProducts();
+            Assert.IsAssignableFrom<Task<IActionResult>>(result);
+        }
     }
 }
