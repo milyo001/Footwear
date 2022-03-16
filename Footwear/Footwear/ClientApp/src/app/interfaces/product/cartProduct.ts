6529d3a7b1f1906458f95695dd640c57909d0d90
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-03-16 15:12:44 +0200

    Add new interface for cart product

---
+++ Footwear/Footwear/ClientApp/src/app/interfaces/product/cartProduct.ts
@@ -0,0 +1,5 @@
+import { IProduct } from "./product";
+
+export interface ICartProduct extends IProduct {
+  productId: number;
+}
--- Footwear/Footwear/ViewModels/ProductViewModel.cs
+++ Footwear/Footwear/ViewModels/ProductViewModel.cs
@@ -14,6 +14,8 @@
 
         public string ProductType { get; set; }
 
+        public int ProductId { get; set; }
+
         public string Gender { get; set; }
 
         public string Size { get; set; }
