a9dfb05b80418c22c90f20f9e95864164400a7c1
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-04-14 16:43:38 +0300

    Implement new configuration class model MailSettings

---
+++ Footwear/Footwear/Services/MailService/MailService.cs
@@ -0,0 +1,19 @@
+﻿using Footwear.Data.Models.Email;
+using System.Threading.Tasks;
+
+namespace Footwear.Services.MailService
+{
+    public class MailService : IMailService
+    {
+        private readonly MailSettings _mailSettings;
+        public MailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
+        public Task SendEmailAsync(EmailRequest mailRequest)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
