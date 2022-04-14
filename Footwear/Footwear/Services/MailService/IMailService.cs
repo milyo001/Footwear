f4c5eb24b00319572ca68f0d850cb2dce34d2c0f
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-04-14 16:40:00 +0300

    Implment new interface IMailService, add docs

---
+++ Footwear/Footwear/Services/MailService/IMailService.cs
@@ -0,0 +1,15 @@
+﻿namespace Footwear.Services.MailService
+{
+    using Footwear.Data.Models.Email;
+    using System.Threading.Tasks;
+
+    public interface IMailService
+    {
+        /// <summary>
+        /// Send an email with Simple Mail Transfer Protocol (SMTP) 
+        /// </summary>
+        /// <param name="mailRequest"></param>
+        /// <returns></returns>
+        Task SendEmailAsync(EmailRequest mailRequest);
+    }
+}
