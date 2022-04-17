7248f5cede9d95b8911c813ced496fff0608e61c
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-04-18 00:33:29 +0300

    Add abstract ApiController class

---
+++ Footwear/Footwear/Controllers/Base/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Footwear.Controllers
+{
+
+    [Route("[controller]")]
+    [ApiController]
+    public abstract class ApiController : ControllerBase
+    {
+       
+    }
+}
