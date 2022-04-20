5b0848abd4a3a715760f8b73111dff72fd1c8f9f
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-04-20 19:33:40 +0300

    Split orders into two arrays of orders

--- Footwear/ClientApp/src/app/orders/orders.component.ts
+++ Footwear/ClientApp/src/app/orders/orders.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { ICompletedOrder } from '../interfaces/order/completedOrder';
 import { OrderService } from '../services/order.service';
-import { 
-  faCalendarDay, faBox, 
-  faCreditCard, faMoneyBill }
-from '@fortawesome/free-solid-svg-icons';
+import {
+  faCalendarDay, faBox,
+  faCreditCard, faMoneyBill
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-orders',
@@ -15,66 +15,77 @@ from '@fortawesome/free-solid-svg-icons';
 export class OrdersComponent implements OnInit {
 
   currentOrders: ICompletedOrder[];
-  completedOrders: ICompletedOrder[];
-  orderId : string;
+  pastOrders: ICompletedOrder[];
+  maxDeliveryDays: number = 0;
+  orderId: string;
   pageIndex: number = 1;
   ordersPerPage: number = 10;
-
 
   // Order statuses recieved from API
   cashPayment: string = 'DeliveryCash';
   cardPayment: string = 'DeliveryPaid';
 
-
   // Icons
   faCalendarDay = faCalendarDay;
   faBox = faBox;
-  // Icons are declared here to render the icons returned as RAW HTML from order-status pipe
   faCreditCard = faCreditCard;
   faMoneyBill = faMoneyBill;
 
   constructor(private orderService: OrderService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    // Get max delivery days
+    this.orderService.getDeliveryPricingData().subscribe(data => {
+      this.maxDeliveryDays = data.maxDelivery;
+    });
+
     this.orderService.getAllOrders().subscribe(orders => {
       // Use "deconstruction" style assignment
-      [this.currentOrders, this.completedOrders] =
+      [this.currentOrders, this.pastOrders] =
         orders
           .reduce((result, element) => {
-            console.log(element.createdOn);
-            var test = new Date(element.createdOn);
-            // Determine and push to current/completed orders array
-            result[element.status == "Completed" ? 1 : 0].push(element);
+            const today = new Date();
+            const orderDate = new Date(element.createdOn);
+            const maxDeliveryDate = this.calculateDeliveryDate(orderDate, this.maxDeliveryDays);
+
+            // If the max delivery date is less than today's date push element to the first array
+            result[maxDeliveryDate < today ? 1 : 0].push(element);
             return result;
           },
-          // By default return empty array, can be further chained with map() or other functions.
-            [[], []]); 
+            // By default return empty array, can be further chained with map() or other functions.
+            [[], []]);
     });
   };
 
-  viewOrder(value: any){
+  viewOrder(value: any) {
     console.log(value);
     console.log(value.toString());
-    
+
   }
 
   sendEmail(order, sendEmailBtn) {
     let id: string = order.orderId;
     sendEmailBtn.disabled = true;
-
     this.orderService.sendEmailForOrder(id).subscribe((response: any) => {
-      if(response.sent){
+      if (response.sent) {
         this.toastr.info("Email sent!");
-        // Disable send button for 5 sec to prevent spammy behavior
+        // Disable send button for 5 sec to prevent spammy/unsolicited behavior
         setInterval(() => {
           sendEmailBtn.disabled = false;
         }, 5000);
       }
     },
-    err => {
-      this.toastr.info("Something went wrong", err.error.message);
-      console.log(err);
-      sendEmailBtn.disabled = false;
-    })
+      err => {
+        this.toastr.info("Something went wrong", err.error.message);
+        console.log(err);
+        sendEmailBtn.disabled = false;
+      })
+  }
+
+  // Returns the actual delivery date
+  calculateDeliveryDate = (orderDate: Date, days: number) => {
+    var date = new Date(orderDate);
+    date.setDate(date.getDate() + days);
+    return date;
   }
 }
