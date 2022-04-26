ae98e62000d34081f964cc1f126a2cbb5f07758b
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-04-26 22:19:38 +0300

    Fixed responsive styles

--- Footwear/ClientApp/src/app/orders/order-details-product/order-details-product.component.ts
+++ Footwear/ClientApp/src/app/orders/order-details-product/order-details-product.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ICartProduct } from 'src/app/interfaces/cart/cartProduct';
 
 @Component({
   selector: 'app-order-details-product',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderDetailsProductComponent implements OnInit {
 
+  @Input() product: ICartProduct;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  test(){
+    console.log(this.product);
+  }
+  
 }
