0e13d44e3d61d32135e9f8ed4800fc0f07d1f32d
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-04-27 22:10:13 +0300

    Add tests

--- Footwear/ClientApp/src/app/orders/order-details/order-details.component.spec.ts
+++ Footwear/ClientApp/src/app/orders/order-details/order-details.component.spec.ts
@@ -22,4 +22,42 @@ describe('OrderDetailsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should declare', () => {
+    expect(component.closeDetailsSection).toBeTruthy();
+  });
+
+  it('should declare', () => {
+    expect(component.deliveryInfo).toBeTruthy();
+  });
+
+  it('should declare', () => {
+    expect(component.detailsToggleEvent).toBeTruthy();
+  });
+
+  it('should declare', () => {
+    expect(component.totalPrice).toBeTruthy();
+  });
+
+  it('should declare', () => {
+    expect(component.totalPrice).toBeTruthy();
+  });
+
+  it('should declare icons', () => {
+    expect(component.faAddress).toBeTruthy();
+    expect(component.faBarcode).toBeTruthy();
+    expect(component.faCalendarAlt).toBeTruthy();
+    expect(component.faCompass).toBeTruthy();
+    expect(component.faHandHoldingUsd).toBeTruthy();
+    expect(component.faMoneyCheckAlt).toBeTruthy();
+    expect(component.faPhoneSquare).toBeTruthy();
+    expect(component.faTruckLoading).toBeTruthy();
+    expect(component.faUser).toBeTruthy();
+    expect(component.faWallet).toBeTruthy();
+  });
+
+  it('should declare', () => {
+    expect(component.panelOpenState).toBeTruthy();
+  });
+
 });
