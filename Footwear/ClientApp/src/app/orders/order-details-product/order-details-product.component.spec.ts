e200dbed17f7c9d44056afd016e172497419f996
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-05-02 23:29:17 +0300

    Add more tests, refactor

--- Footwear/ClientApp/src/app/orders/order-details-product/order-details-product.component.spec.ts
+++ Footwear/ClientApp/src/app/orders/order-details-product/order-details-product.component.spec.ts
@@ -1,25 +1,46 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { ICartProduct } from 'src/app/interfaces/cart/cartProduct';
 import { OrderDetailsProductComponent } from './order-details-product.component';
 
 describe('OrderDetailsProductComponent', () => {
+  const product: ICartProduct = {
+    size: 20,
+    details: 'sadas',
+    name: 'test',
+    gender: 'undefined',
+    price: 20,
+    imageUrl: 'google.com/image?=21313asd',
+    productId: 1,
+    quantity: 2,
+    id: 44,
+    productType: 'running',
+  };
+
   let component: OrderDetailsProductComponent;
   let fixture: ComponentFixture<OrderDetailsProductComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ OrderDetailsProductComponent ]
-    })
-    .compileComponents();
+      declarations: [OrderDetailsProductComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(OrderDetailsProductComponent);
     component = fixture.componentInstance;
+    component.product = product;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should define @Input #product', () => {
+    expect(component.product).toBeDefined();
+  });
+
+  it('should be product of type ICartProduct', () => {
+    expect(component.product).toEqual(product);
   });
 });
