9b088de29444d7069e966377b0ea4ee1e97e91e8
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-05-09 23:37:10 +0300

    Code clean up

--- Footwear/ClientApp/src/app/services/order.service.ts
+++ Footwear/ClientApp/src/app/services/order.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { getAPIUrl } from 'src/environments/environment';
@@ -8,30 +8,32 @@ import { IDeliveryInfo } from '../interfaces/order/deliveryInfo';
 import { IOrder } from '../interfaces/order/order';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 
 export class OrderService {
-
   private apiUrl: string = getAPIUrl();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   // Redirect user to the stripe payment page
   checkOut() {
-    return this.http.get(this.apiUrl + "payments/create-checkout-session");
+    return this.http.get(this.apiUrl + 'payments/create-checkout-session');
   }
 
   // Send the order to the server in the body
   createOrder(order: IOrder) {
-    const headers = new HttpHeaders()
-      .set('Content-Type', 'application/json');
-    return this.http.post(this.apiUrl + "order/create-order", order, { 'headers': headers });
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this.http.post(this.apiUrl + 'order/create-order', order, {
+      headers: headers,
+    });
   }
 
   // Send request to the API to validate if the payment was successfull
   validatePayment(sessionId: string) {
-    return this.http.get(this.apiUrl + 'payments/payment-success/?session_id=' + sessionId);
+    return this.http.get(
+      this.apiUrl + 'payments/payment-success/?session_id=' + sessionId
+    );
   }
 
   // Get the delivery data, example: delivery price and delivery time
@@ -47,15 +49,17 @@ export class OrderService {
   // Send Email with order details
   sendEmailForOrder(id: string) {
     const body = { id };
-    return this.http.post(this.apiUrl + 'email/send/', body,
-    { headers: { 'Content-Type': 'application/json' }});
+    return this.http.post(this.apiUrl + 'email/send/', body, {
+      headers: { 'Content-Type': 'application/json' },
+    });
   }
 
   // Calculates the total price for order's cart products, including the price for delivery
   calculateTotalPrice(products: ICartProduct[], deliveryPrice: number) {
-    let price = products.reduce
-      ((total: number, product: ICartProduct) => total + (product.price * product.quantity), 0);
+    let price = products.reduce(
+      (total: number, product: ICartProduct) =>
+        total + product.price * product.quantity, 0);
     price += deliveryPrice;
     return price;
-    };
+  }
 }
