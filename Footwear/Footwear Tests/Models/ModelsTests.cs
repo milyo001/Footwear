5e0a7b0ee844e92fae5b4cc9e17c9884865d39f4
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-06-27 15:25:07 +0300

    [UnitTest] Add tests for validators in Address model

---
+++ Footwear/Footwear Tests/Models/ModelsTests.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace Footwear_Tests.Models
+{
+    using Footwear.Data.Models;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using Xunit;
+
+    public class ModelsTests
+    {
+        //Unit Test DataAnnotations method
+        private IList<ValidationResult> ValidateModel(object model)
+        {
+            var validationResults = new List<ValidationResult>();
+            var ctx = new ValidationContext(model, null, null);
+            Validator.TryValidateObject(model, ctx, validationResults, true);
+            return validationResults;
+        }
+
+        [Fact]
+        public void Test_Correct_Address_Model_Validations()
+        {
+            var correctAddress = new Address
+            {
+                Id = 1,
+                State = "Sofia",
+                City = "Sofia",
+                Street = "Test street 22B",
+                Country = "Bulgaria",
+                ZipCode = "1000"
+            };
+
+            var errors = ValidateModel(correctAddress);
+            Assert.True(errors.Count == 0);
+        }
+    }
+}
