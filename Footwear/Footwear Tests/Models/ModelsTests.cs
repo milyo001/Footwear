5b85dbbe4c583661245a0f79e3302f21f2903fd5
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-06-28 22:28:50 +0300

    Add tests for Cart model

--- Footwear/Footwear Tests/Models/ModelsTests.cs
+++ Footwear/Footwear Tests/Models/ModelsTests.cs
@@ -487,5 +487,36 @@ namespace Footwear_Tests.Models
             Assert.True(errors.Count == 1);
         }
 
+        // Cart model
+
+        [Fact]
+        public void Test_Cart_Model_If_Working_AsExpected()
+        {
+            var cart = new Cart
+            {
+                Id = 1,
+                UserId = "sadwqae23918kjkclaskdj",
+                CartProducts = new List<CartProduct>()
+            };
+
+            var errors = ValidateModel(cart);
+            Assert.True(errors.Count == 0);
+        }
+
+        
+        [Fact]
+        public void Test_Cart_Model_When_UserId_IsNull()
+        {
+            var cart = new Cart
+            {
+                Id = 1,
+                UserId = null,
+            };
+
+            var errors = ValidateModel(cart);
+            Assert.True(errors.Count == 1);
+        }
+
+        
     }
 }
